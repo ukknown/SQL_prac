f7a8c5100ed53c98d96c1452e772a9ae5d33896c
Jonguk Choi <87683992+ukknown@users.noreply.github.com>
2023-07-27 22:02:06 +0900

    Create Solution.sql

---
+++ programmers/조건에_맞는_회원수_구하기/Solution.sql
@@ -0,0 +1,3 @@
+//USER_INFO 테이블에서 2021년에 가입한 회원 중 나이가 20세 이상 29세 이하인 회원이 몇 명인지 출력하는 SQL문을 작성해주세요.
+
+SELECT COUNT(USER_ID) AS USERS FROM USER_INFO WHERE YEAR(JOINED) = 2021 AND AGE >= 20 AND AGE <= 29;
